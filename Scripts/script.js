8b0dc83bcffc17eca1b270728a850801b3ab7b44
TIAGO NEVES LEAO COSTA LINO <tiagobrasil18@gmail.com>
2023-11-05 08:55:15 -0300

    [lesson/37] - example

---
+++ Scripts/script.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular
+            .module("Demo", [])
+            .controller("redColourController", function ($scope, $rootScope) {
+                $scope.redColour = "I am Red Colour";
+                $rootScope.rootScopeColour = "I am Root Scope Colour";
+            })
+            .controller("greenColourController", function ($scope) {
+                $scope.greenColour = "I am Green Colour";
+            })
